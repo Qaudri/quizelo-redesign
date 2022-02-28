53c9d47b116e556a5280656093e5d70de3bd1bbd
Qaudri <aquadirmuhammad@gmail.com>
2022-02-28 13:51:18 +0100

    refactor: navbar created

--- pages/quiz_page.vue
+++ pages/quiz_page.vue
@@ -18,29 +18,7 @@
         <div class="container mx-auto">
 
           <div class="my-4 w-full absolute bottom-0 px-6 md:px-0 flex justify-between container mx-auto">
-
-            <div class="flex bg-accent3-300 p-2 hover:bg-transparent hover:border-accent3-400 border-2">
-              <img :src="require('@/assets/images/backward.svg')" alt="" class="mr-2" />
-              <p class="font-medium text-accent3-600 mt-1">Previous</p>
-            </div>
-        
-            <div><img :src="require('@/assets/images/clock.svg')" alt="">
-            </div>
-        
-            <div class="flex">
-              <div class="flex bg-accent1-600 p-2 hover:bg-transparent hover:border-accent1-600 border-2">
-                <p class="font-medium text-white mr-2 hover:text-accent1-600">Next</p>
-                <img :src="require('@/assets/images/forward.svg')" alt="">
-              </div>
-
-              <div class="flex mx-2 p-2">
-                <p class="font-medium text-accent1-600 mr-2">Skip</p>
-                <img :src="require('@/assets/images/fast-forward.svg')" alt="">
-              </div>
-            </div>
-      
-
-
+            <SectionsQuizNavbar />
           </div>
 
         </div>
