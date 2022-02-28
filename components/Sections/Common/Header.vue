5802904c15cc02f7dff40825c0b34f740901c812
Qaudri <aquadirmuhammad@gmail.com>
2022-02-28 13:08:01 +0100

    refactor: menu items component added

--- components/Sections/Common/Header.vue
+++ components/Sections/Common/Header.vue
@@ -1,8 +1,9 @@
 <template>
   <section class="container mx-auto px-6 md:px-0">
+
     <div class="block md:flex justify-between items-center py-4 bg-white ">
 
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <div >
           <img  :src="require('@/assets/images/quizelo.svg')" class="w-32" />
         </div>
@@ -18,25 +19,10 @@
         
       </div>
 
-      <div :class="is_menu_open ? 'block' : 'hidden'" class="items-center md:m-0 pt-4 md:flex md:h-auto h-screen-90 md:bg-transparent bg-white position md:max-w-max w-full">
-        <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
-          How it works?
-        </a>
-  
-        <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
-          Features
-        </a>
-  
-        <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
-          About Us
-        </a>
-  
-        <a href="#"
-          class=" mx-6 cursor-pointer md:mx-8 border bg-gradient-to-r from-accent1-500 to-accent2-500  px-6 py-3 uppercase text-white rounded-lg font-medium">
-          Login
-        </a>
-      </div>
-  
+      <UiSharedMenu :class="is_menu_open ? 'block' : 'hidden'" 
+      class="items-center md:p-0 pt-4 md:flex md:h-auto h-screen-90 md:bg-transparent bg-white position md:max-w-max w-full"
+       />
+
     </div>
     <div class="md:border-b-2"></div>
   </section>
---
+++ components/Ui/Shared/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="block md:flex items-center">
+      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
+        How it works?
+      </a>
+
+      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
+        Features
+      </a>
+
+      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
+        About Us
+      </a>
+
+      <a href="#"
+        class=" mx-6 cursor-pointer md:mx-8 border bg-gradient-to-r from-accent1-500 to-accent2-500  px-6 py-3 uppercase text-white rounded-lg font-medium">
+        Login
+      </a>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
