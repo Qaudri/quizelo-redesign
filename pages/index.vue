a145d21e29431106fcb317732c7e9c486b812e3a
Qaudri <aquadirmuhammad@gmail.com>
2022-02-28 20:39:28 +0100

    refactor: changed router link

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,14 @@
     <SectionsCommonHeader />
 
     <section class="mx-auto container">
+
       <div class=" flex items-center">
+
         <div class="w-full">
+
           <div class="lg:flex justify-between px-6 md:px-10 2xl:p-0">
             <div class="w-full lg:w-1/2">
+            
               <div class="pt-16 text-4xl md:text-6xl font-bold">
                 <div>
                   <h2>Learn <br> new concepts <br>for each question</h2>
@@ -19,7 +23,7 @@
               </div>
 
               <div class="flex mt-8">
-                <a href="/quiz_page"
+                <a href="/start_game"
                   class="cursor-pointer border-2 bg-gradient-to-r from-accent1-500 to-accent2-500  px-4 py-3 uppercase text-white rounded-lg font-medium">
                   Start Solving
               </a>
