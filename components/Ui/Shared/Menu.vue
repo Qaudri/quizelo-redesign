3527eb44b45cd45d482320201ee1ce8aa35cfbb0
Qaudri <aquadirmuhammad@gmail.com>
2022-03-02 11:34:42 +0100

    refactor: menu items

--- components/Ui/Shared/Menu.vue
+++ components/Ui/Shared/Menu.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
     <div class="block md:flex items-center">
-      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
+      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 font-medium block border-b-2 md:border-none hover:text-accent1-600 duration-200 ease-in-out">
         How it works?
       </a>
 
-      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
+      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 font-medium block border-b-2 md:border-none hover:text-accent1-600 duration-200 ease-in-out">
         Features
       </a>
 
-      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
+      <a href="#" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 font-medium block border-b-2 md:border-none hover:text-accent1-600 duration-200 ease-in-out">
         About Us
       </a>
 
-      <a href="/Faq" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 block border-b-2 md:border-none">
+      <a href="/Faq" class="pb-2 px-6 md:p-0 my-6 md:my-0 md:mx-6 font-medium block border-b-2 text-base md:border-none hover:text-accent1-600 duration-200 ease-in-out">
         FAQs
       </a>
 
       <a href="#"
-        class=" mx-6 cursor-pointer md:mx-8 border bg-gradient-to-r from-accent1-500 to-accent2-500  px-6 py-3 uppercase text-white rounded-lg font-medium">
+        class=" mx-6 cursor-pointer md:mx-8 border bg-gradient-to-r from-accent1-500 to-accent2-500 text-lg  px-7 py-3 uppercase text-white rounded-lg font-medium">
         Login
       </a>
 
