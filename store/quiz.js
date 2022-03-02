a512a8b391ee337634eb45e81117aa8d452e8219
Qaudri <aquadirmuhammad@gmail.com>
2022-03-02 16:07:10 +0100

    chore: filled in question array

---
+++ store/quiz.js
@@ -0,0 +1,40 @@
+export const state = () => ({
+  questions: [
+    {
+      id: 1,
+      question: "Who created the HTMl language",
+      options: [
+        "A-Tim Bernerslee", "B-John Doe", "C-Tim Bernersly", "D-Chris Do"
+      ],
+      right_option_index: 0,
+    },
+
+    {
+      id: 2,
+      question: "In programming, what's the common practice sentence?",
+      options: [
+        "A-Good morning", "B-Hello World!", "C-How are you?", "D-What's up?"
+      ],
+      right_option_index: 1,
+    },
+
+    {
+      id: 3,
+      question: "Which of the following software supports team work?",
+      options: [
+        "A-Canva", "B-Adobe Photoshop", "C-Flash", "D-Figma"
+      ],
+      right_option_index: 3,
+    },
+
+    {
+      id: 4,
+      question: "In development, what sign represents the mathematical sybol for multiplication?",
+      options: [
+        "A-# (Hash symbol)", "B-* (Asterisk)", "C-& (And symbol)", "D-x(Letter x)"
+      ],
+      right_option_index: 1,
+    },
+    
+  ],
+}),
