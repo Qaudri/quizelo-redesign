40148c23c1dd100c331b881d7aa129051ff2dbcc
Qaudri <aquadirmuhammad@gmail.com>
2022-03-03 15:55:15 +0100

    chore: added animation to overlay page design

--- components/Sections/Quizcomplete/Overlay.vue
+++ components/Sections/Quizcomplete/Overlay.vue
@@ -1,6 +1,7 @@
-<template>
-  <div class="flex items-center justify-center container mx-auto py-6 h-screen">
-    <div>
+<template >
+  <div :class="is_fading ? 'fade-out' : ''"
+  class=" flex items-center py-10 justify-center h-screen">
+    <div class="">
 
       <div class="flex justify-center">
         <img :src="require('@/assets/images/congratulations.svg')" width="65%" alt="">
@@ -15,14 +16,14 @@
         </div>
       </div>
 
-      <div class="flex justify-center w-full my-12">
+      <div class="flex justify-center w-full mt-12">
         <div class="flex justify-evenly items-center w-full">
 
           <a href="/" class="font-medium text-lg mx-8 hover:text-accent1-600 ease-in-out duration-500">
           Back to home page
           </a>
 
-          <button @click="emitViewResult"
+          <button @click="emitViewResults" 
            class="mx-8 text-white rounded-xl  font-medium text-lg bg-accent1-600 py-2 px-4 hover:text-accent1-600 hover:bg-transparent hover:border-accent1-600 
           border-transparent border-2 duration-500 ease-in-out">
             View your results
@@ -35,13 +36,33 @@
 
 <script>
 export default {
+  data(){
+    return{
+      is_fading: false,
+    }
+  },
+
   methods: {
     emitViewResults(){
-      this.$emit("viewResults")
+      this.is_fading = true;
+      this.$emit("viewResults");
     }
   }
 }
 </script>
 
 <style>
+.fade-out{
+  animation: fadeOut 1s forwards;
+}
+
+@keyframes fadeOut {
+  from{
+    opacity: 100%;
+  }
+
+  to{
+    opacity: 0%;
+  }
+}
 </style>
