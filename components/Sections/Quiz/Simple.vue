aa8d6728a102a7cfbd9ffacb7ed962b107ed2db9
Qaudri <aquadirmuhammad@gmail.com>
2022-03-10 21:24:52 +0100

    fix: fixed issues with pop-up dialog

--- components/Sections/Quiz/Simple.vue
+++ components/Sections/Quiz/Simple.vue
@@ -1,7 +1,7 @@
 <template>
   <div class=" absolute bg-accent3-600 bg-opacity-80 flex items-center justify-center h-full w-full">
 
-    <div class="bg-white rounded-xl p-6 h-32 w-1/2">
+    <div class="bg-white rounded-xl p-6 h-1/2 w-1/2">
 
       <div>
         <div class="border-b text-xl pb-4 font-semibold flex justify-between">
@@ -18,15 +18,15 @@
 
         </div>
 
-        <div class="my-4 flex items-center justify-center w-full">
+        <div class="my-6 flex items-center justify-center">
           <div>
-            <img :src="require('@/assets/images/important.svg')" class="w-20 flex items-center justify-center m-0" />
+            <img :src="require('@/assets/images/important.svg')"  class="flex items-center mx-auto justify-center m-0 w-32" />
 
             <p class="text-black font-medium text-xl text-center max-w-sm flex justify-center">Are you sure you want to submit? This action is irreversible!</p>
+
           </div>
 
         </div>
-        
         
       </div>
 
