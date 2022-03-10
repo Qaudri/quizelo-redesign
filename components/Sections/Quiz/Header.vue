45a64bde4971779fbe51b0053f2a77c3f53b7aad
Qaudri <aquadirmuhammad@gmail.com>
2022-03-10 16:30:11 +0100

    chore: created quiz header.vue

---
+++ components/Sections/Quiz/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex justify-between py-3 container mx-auto">
+    <UiSharedLogo />
+
+    <a href="#"
+      class=" mx-6 cursor-pointer md:mx-8 border bg-gradient-to-r from-accent1-500 to-accent2-500 text-lg  px-7 py-3 uppercase text-white rounded-lg font-medium">
+      Submit quiz
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
