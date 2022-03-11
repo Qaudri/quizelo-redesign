b5a6f58a15fc4905986be67da51e3684148ca648
Qaudri <aquadirmuhammad@gmail.com>
2022-03-11 10:52:22 +0100

    refactor: quiz page components

--- pages/quiz_page.vue
+++ pages/quiz_page.vue
@@ -1,44 +1,38 @@
 <template >
-  <div class="flex justify-center h-screen">      
-    <div class="w-full my-20">
+  <div class="">  
+
+    <div class="container mx-auto ">
+      <SectionsQuizHeader class="" @ModalDialog="openDialog" />
+    </div>
+
+    <SectionsQuizSimple :class="dialog_open ? 'block z-150 top-0' : 'hidden' " @closeDialog="closeDialog"/>
+
+    <div class="w-full flex justify-center items-center">
       <div class=""> 
-        <!-- <div class="">
-          <SectionsQuizHeader class=" top-0 w-full" />
-        </div> -->
 
-        <div class="container mx-auto relative flex h-fit justify-center items-center mb-10 w-full">
-          <SectionsQuizHeader class="w-full flex justify-between absolute" @ModalDialog="openDialog" />
-        </div>
-
-        <SectionsQuizSimple :class="dialog_open ? 'block z-150 top-0' : 'hidden' " @closeDialog="closeDialog"/>
-
-        <!-- <div class="container mx-auto relative flex h-fit justify-center items-center mb-10 w-full h-1 bg-accent1-600">
-          <SectionsQuizNumberlist class="w-full flex justify-between absolute" />
-        </div> -->
+        <SectionsQuizNumberlist class="w-full container mx-auto overflow-x-hidden flex justify-between my-6" />
 
         <div class="bg-accent1-300 bg-opacity-20 p-6 w-screen mx-auto">
           <SectionsQuizQuestion :question="question.question"  />
         </div>
 
-      </div>
-      
-
-      <div class="px-6 md:px-0 grid grid-cols-2 container mx-auto gap-6 mt-14">
-        <SectionsQuizAnswer2  @OptionChosen="userChooseOption(answer)" v-for="answer in question.options" :key="answer.option_id"
-        :selected="answer.selected" :option_text="answer.option_text" :option_tag="answer.option_tag" />
-      </div>
-      
-      <div class="container mx-auto">
-
-        <div class="mb-4 w-full absolute bottom-0 px-14 md:px-0 flex justify-between container mx-auto">
-          <SectionsQuizNavbar @timeOver="showNextQuestion"
-          @PreviousQuestion="showPreviousQuestion" @NextQuestion="showNextQuestion" />
+        <div class="px-6 md:px-0 grid grid-cols-2 container mx-auto gap-6 mt-14">
+          <SectionsQuizAnswer2  @OptionChosen="userChooseOption(answer)" v-for="answer in question.options" :key="answer.option_id"
+          :selected="answer.selected" :option_text="answer.option_text" :option_tag="answer.option_tag" />
         </div>
 
       </div>
 
     </div>
-      
+    
+    <div class="container mx-auto">
+
+      <div class="mb-4 w-full absolute bottom-0 px-14 md:px-0 flex justify-between container mx-auto">
+        <SectionsQuizNavbar @timeOver="showNextQuestion"
+        @PreviousQuestion="showPreviousQuestion" @NextQuestion="showNextQuestion" />
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -105,7 +99,7 @@ export default {
 </script>
 <style>
 body{
-  position: relative;
+  /* position: relative; */
   width: 100%;
   height: 100%;
 }
