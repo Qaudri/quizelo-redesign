6b221a15e348c5a4db3a334a60c126aaf697a525
Qaudri <aquadirmuhammad@gmail.com>
2022-03-16 17:29:00 +0100

    chore: created summary question

---
+++ components/Sections/Quizcomplete/Question.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <img :src="require('@/assets/images/correct.svg')" alt="">
+        <img :src="require('@/assets/images/wrong.svg')" alt="">
+      </div>
+
+      <div class="font-medium text-black text-lg">
+        This is the question
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
