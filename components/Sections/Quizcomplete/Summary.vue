912184a4354087486c495315494ed46169aa957a
Qaudri <aquadirmuhammad@gmail.com>
2022-03-16 18:41:59 +0100

    refactor: minor ui issues fixed

--- components/Sections/Quizcomplete/Summary.vue
+++ components/Sections/Quizcomplete/Summary.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-white mt-3 px-6 py-3 flex justify-between">
-    <div class="flex items-center w-1/2">
+  <div class="bg-white mt-3 px-6 py-3 flex justify-between items-center">
+    <div class="flex items-center w-2/3">
 
       <div class="w-10 mr-3">
         <img v-if="is_correct" :src="require('@/assets/images/correct.svg')" alt="">
@@ -12,12 +12,12 @@
       </div>
     </div>
 
-    <div>
-      <div>
+    <div class="">
+      <div class="px-4 py-2 border">
         <div class="text-black font-medium text-lg">Correct option: <span class="text-black font-normal text-lg"> {{correct_option}} </span> </div>
       </div>
 
-      <div>
+      <div class="px-4 py-2 border">
         <div class="text-black font-medium text-lg">You choosed: <span class="text-black font-normal text-lg"> {{choosen_option}} </span> </div>
       </div>
   </div>
