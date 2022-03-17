b2c43cea4c6b986e005b31e094d6b8222a1890d7
Qaudri <aquadirmuhammad@gmail.com>
2022-03-17 20:13:07 +0100

    quiz page header component responsiveness

--- components/Sections/Quiz/Header.vue
+++ components/Sections/Quiz/Header.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex justify-between py-3 container mx-auto">
+  <div class="px-8 md:px-0 flex items-center justify-between py-3 container mx-auto">
     <UiSharedLogo />
 
     <a href="#" @click="emitModalEvent"
-      class=" mx-6 cursor-pointer md:mx-8 border bg-gradient-to-r from-accent1-500 to-accent2-500 text-lg  px-7 py-3 uppercase text-white rounded-lg font-medium">
+      class=" cursor-pointer md:mx-8 border bg-gradient-to-r from-accent1-500 to-accent2-500 text-lg  px-7 py-3 uppercase text-white rounded-lg font-medium">
       Submit quiz
     </a>
   </div>
