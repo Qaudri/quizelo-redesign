9b01194b0af0119de766b88d8f720bdae404f213
Qaudri <aquadirmuhammad@gmail.com>
2022-03-17 20:32:32 +0100

    refactor: quiz summary page responsiveness

--- components/Sections/Quizcomplete/Summary.vue
+++ components/Sections/Quizcomplete/Summary.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="bg-white mt-3 px-6 py-3 flex justify-between items-center">
-    <div class="flex items-center w-2/3">
+  <div class="bg-white mt-3 px-6 py-3 md:flex justify-between items-center">
+    <div class="flex items-center">
 
-      <div class="w-10 mr-3">
+      <div class="w-1/12 md:w-10 mr-3 p-0">
         <img v-if="is_correct" :src="require('@/assets/images/correct.svg')" alt="">
         <img v-else :src="require('@/assets/images/wrong.svg')" alt="">
       </div>
 
-      <div class="font-medium text-black text-lg">
+      <div class="w-11/12 md:w-full font-medium text-black text-lg">
         {{theQuestion}}
       </div>
     </div>
 
-    <div class="">
+    <div class="mt-4 md:mt-0">
       <div class="px-4 py-2 border">
         <div class="text-black font-medium text-lg">Correct option: <span class="text-black font-normal text-lg"> {{correct_option}} </span> </div>
       </div>
