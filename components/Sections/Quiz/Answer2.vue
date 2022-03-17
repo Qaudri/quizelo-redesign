25787c56ddc4b94fde0a79d4c693a947606583c7
Qaudri <aquadirmuhammad@gmail.com>
2022-03-17 20:07:28 +0100

    refactor: answer component responsiveness

--- components/Sections/Quiz/Answer2.vue
+++ components/Sections/Quiz/Answer2.vue
@@ -2,7 +2,7 @@
   <div class="select-none" >
     <div :class="selected ? 'bg-accent1-500' : 'bg-accent3-200' " @click="emitChooseOption" class="relative rounded-2xl md:px-14 px-12 py-8 md:py-10" >
         <p :class="selected ? 'text-white' : 'text-accent3-400' " class=" font-medium text-sm absolute top-3 left-4 ">{{option_tag}}</p>
-        <h1 :class="selected ? 'text-white' : 'text-black' " class="text-accent3-700 font-medium text-md md:text-xl md:px-8 uppercase">{{option_text}}</h1>
+        <h1 :class="selected ? 'text-white' : 'text-black' " class="text-accent3-700 font-medium text-md md:text-xl uppercase">{{option_text}}</h1>
     </div>
 
   </div>
