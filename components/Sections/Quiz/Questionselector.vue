770aaddc1636500c588748c21d74140addf136f9
Qaudri <aquadirmuhammad@gmail.com>
2022-03-17 20:02:51 +0100

    refactor: focus mode removed

--- components/Sections/Quiz/Questionselector.vue
+++ components/Sections/Quiz/Questionselector.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div @click="emitQuestionNumber(item)" v-for="item in question_no" :key="item" :class="is_focused ? 'bg-accent1-600 text-white border-0' : 'bg-white border-2 border-accent1-600 text-black' "
-    class="select-none mx-4 h-10 w-10 rounded-lg flex items-center justify-center  font-medium">
+    <div @click="emitQuestionNumber(item)" v-for="item in question_no" :key="item"
+    class="select-none mx-4 h-10 w-10 rounded-lg flex items-center justify-center bg-white border-2 border-accent1-600 text-black font-medium">
       {{item}}
     </div>
   </div>
