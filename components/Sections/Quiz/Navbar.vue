0d14f1c1fde2dc665b63d71fb9d125bfd3903fd8
Qaudri <aquadirmuhammad@gmail.com>
2022-03-21 17:20:12 +0100

    chore: created news page for api consumption

--- components/Sections/Quiz/Navbar.vue
+++ components/Sections/Quiz/Navbar.vue
@@ -46,6 +46,32 @@
 
 <script>
 export default {
+  data() {
+      return {
+          countDown: 30
+      }
+  },
+
+  watch: {
+
+    countDown: {
+      handler(value) {
+
+        if (value > 0) {
+          setTimeout(() => {
+            this.countDown--;
+          }, 1000);
+        }
+
+        else if (value === 0) {
+          this.$emit("timeOver");
+        }
+
+      },
+      immediate: true // This ensures the watcher is triggered upon creation
+    }
+
+  },
 
   methods: {
     emitNextEvent(){
---
+++ pages/news.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <SectionsCommonHeader />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
